ad14697ebe4ad3e56bfd5a63445c48b974440a89
Simon Seo <simonseo.doubles@gmail.com>
2022-03-14 12:47:14 +0900

    remove prompt call

--- cordova-js-src/android/promptbasednativeapi.js
+++ cordova-js-src/android/promptbasednativeapi.js
@@ -24,7 +24,7 @@
 
 module.exports = {
     exec: function(bridgeSecret, service, action, callbackId, argsJson) {
-        return prompt(argsJson, 'gap:'+JSON.stringify([bridgeSecret, service, action, callbackId]));
+        return null; // prompt(argsJson, 'gap:'+JSON.stringify([bridgeSecret, service, action, callbackId]));
     },
     setNativeToJsBridgeMode: function(bridgeSecret, value) {
         prompt(value, 'gap_bridge_mode:' + bridgeSecret);
